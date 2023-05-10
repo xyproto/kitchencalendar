4553bcea40068bf1b47c4367b342babf1315cc96
Alexander F. Rødseth <rodseth@gmail.com>
2023-05-10 15:03:14 +0200

    Add a friendly message

---
+++ none.go
@@ -0,0 +1,17 @@
+//go:build !nb_NO
+
+package main
+
+import (
+	"errors"
+	"time"
+
+	"github.com/xyproto/kal"
+)
+
+const msg = "select a locale when building, for example: go build -tags nb_NO"
+
+func formatDate(cal kal.Calendar, date time.Time) string { return msg }
+func weekString(week int) string                         { return msg }
+func dayAndDate(cal kal.Calendar, t time.Time) string    { return msg }
+func newCalendar() (kal.Calendar, error)                 { return nil, errors.New(msg) }
